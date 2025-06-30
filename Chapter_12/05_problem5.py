688563cb51fabeca1297cc57a7e92b3108b261e0
milancodess <milanbhandari153@gmail.com>
2025-06-30 21:15:12 +0545

    Renamed Project_12 to Chapter_12

--- Chapter_12/05_problem5.py
+++ Chapter_12/05_problem5.py
@@ -4,4 +4,4 @@ n = int(input("Enter a number: "))
 table = [n*i for i in range(1, 11)]
 print(table)
 with open("tables.txt","a") as f:
-    f.write(str(table) + "\n")
+    f.write(f"Table of {n}: {str(table)}\n")
